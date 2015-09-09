102e01976b102a6137da4c1cc3ef5e4f75d552b8
tate_fan <yongtao.fan@adleida.com>
2015-09-09 12:47:23 +0800

    modify editorconfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,24 @@
 /* Grunt file */
 
 module.exports = function(grunt) {
+
     grunt.initConfig({
-        jshint: {}
+        jshint: {
+            "files": ['Gruntfile.js', 'app/public/js/*.js'],
+            "jshintrc": ".jshintrc"
+        },
+        watch: {
+            script: {
+                files: ['app/public/js/*.js', 'Gruntfile.js'],
+                options: {
+                    event: ['all']
+                },
+                tasks: ['jshint']
+            }
+        }
     });
-    grunt.loadNpmTasks("grunt-contrib-jshint");
+
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.registerTask("default", ['watch']);
 };
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Kugou linux desktop app
 
 1. install
 
-    ```
+    ```bash
     $ git clone git@bitbucket.org:Tate_fan/kugou.git
     $ make install
     $ make build
